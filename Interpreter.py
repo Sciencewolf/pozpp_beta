b30807cbbe330c597fc51ad8242f2dff721238e9
Sciencewolf <aron17marton@gmail.com>
2022-12-10 21:03:28 +0100

    Removed os.getcwd()

--- Interpreter.py
+++ Interpreter.py
@@ -1,30 +1,29 @@
-import os, sys
+import os
 
 print("""
 Welcome to Pozakarpatsky++ programming language
-Version 0.1
+Version 0.4
 Project started: 11/28/2022
-
 """)
 
 
 class Interpreter:
     print("""
-    Interpreter called
+        Interpreter called
     """)
 
     def __init__(self):
-        self.path = os.getcwd()  # Get current working directory
+        # self.path = os.getcwd()   Get current working directory
         self.ls = []  # List
 
     def open_pozpp_file(self, file_name: str):
         if not file_name.endswith("pozpp"):
             print("Error: File extension is not correct")
         else:
-            file_pozpp = open(f"{self.path}\\{file_name}", 'r')
+            file_pozpp = open(f"{file_name}", 'r')
             self.ls = file_pozpp.readlines()
 
-        file = open(f"{self.path}\\{file_name}", 'w')
+        file = open(f"{file_name}", 'w')
 
         for line in self.ls:
             file.write(line)
