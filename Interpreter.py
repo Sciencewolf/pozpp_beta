7ad55f7c3f8a0e4f45f2f892f8976f8f607e147c
Sciencewolf <aron17marton@gmail.com>
2022-12-10 20:37:21 +0100

    Initial commit

--- Interpreter.py
+++ Interpreter.py
@@ -14,28 +14,19 @@ class Interpreter:
     """)
 
     def __init__(self):
-        self.path = os.getcwd()
-        self.KEYWORDS = {
-            "chislo": "def",
-            "holovna": "main",
-            "kity": "if",
-            "kity_net": "else",
-            "ajbo": "elif",
-            "nekaj": "print",
-            "vhod": "input"
-        }
+        self.path = os.getcwd()  # Get current working directory
+        self.ls = []  # List
 
     def open_pozpp_file(self, file_name: str):
         if not file_name.endswith("pozpp"):
             print("Error: File extension is not correct")
         else:
             file_pozpp = open(f"{self.path}\\{file_name}", 'r')
-            ls = file_pozpp.readlines()
+            self.ls = file_pozpp.readlines()
 
         file = open(f"{self.path}\\{file_name}", 'w')
 
-        for line in ls:
+        for line in self.ls:
             file.write(line)
 
         return file.name
-
