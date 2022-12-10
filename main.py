9cc133d04b57500741ef119b0c56c2603623ae92
Sciencewolf <aron17marton@gmail.com>
2022-12-10 21:05:56 +0100

    Added if's to check return code

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from Run import Run
+import sys
 
 if __name__ == "__main__":
-    return_code = Run.combine_all()
-    print(return_code)
+    print("""
+        main called
+    """)
 
+    file_name = sys.argv[1]
 
+    run = Run()
+    return_code = Run.combine_all(run, file_name)
+
+    if return_code == 0:
+        print("Completed successfully")
+    elif return_code == 1:
+        print("Error: Code is not executed")
