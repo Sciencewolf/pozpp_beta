98e47535b3772c373c0ea7e1f051b846bd497f6b
Sciencewolf <aron17marton@gmail.com>
2022-12-11 20:53:38 +0100

    Added status for return code's

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from Build import Build
 import sys
 
 if __name__ == "__main__":
-    print("""
-        main called
-    """)
+    ''' print("""
+    # main called
+    # """, end="") '''
 
     file_name = sys.argv[1]
     print("Main: file_name", file_name)
@@ -14,5 +14,7 @@ if __name__ == "__main__":
 
     if return_code == 0:
         print("Completed successfully")
+        print("Status: --noerror")
     elif return_code == 1:
         print("Error: Code is not executed")
+        print("Status: --error")
