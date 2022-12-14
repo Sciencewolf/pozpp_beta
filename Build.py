559f17eb5d70c603c128f0ce63e2127b11e203ba
Sciencewolf <aron17marton@gmail.com>
2022-12-14 22:16:42 +0100

    Added colors

--- Build.py
+++ Build.py
@@ -4,23 +4,24 @@ import os
 
 
 class Build:
-    '''print("""
-        Build called
-    """, end="")'''
+    """ print(
+        "Build called"
+    , end="") """
 
     def __init__(self):
         self.interpreter = Interpreter()
         self.preprocess = Preprocess()
 
+        self.WARNING = '\033[93m'
+        self.ENDC = '\033[0m'
+        self.BLUE = '\033[94m'
+
     def combine_all(self, file_name) -> int:
         path = self.interpreter.open_pozpp_file(file_name)
         result = self.preprocess.convert_pozpp_to_py(path)
-        # Debug
-        #print("path: ", path)
-        #print("result", result)
-        print("-" * 20)
-        print("Expected errors below")
-        # End
+
+        print(self.BLUE + "-" * 20 + self.ENDC)
+        print(self.WARNING + "Expected errors below" + self.ENDC)
 
         if os.system(f'python {result}') == 0:
             return 0
