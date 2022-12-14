e533fc1268a3b97b62c8339f4ec30815d5a9231e
Sciencewolf <aron17marton@gmail.com>
2022-12-14 22:16:13 +0100

    Added colors and counting argv's

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from Build import Build
 import sys
+import os
 
 if __name__ == "__main__":
-    ''' print("""
-    # main called
-    # """, end="") '''
+    WARNING = '\033[93m'
+    ENDC = '\033[0m'
 
-    file_name = sys.argv[1]
-    print("Main: file_name", file_name)
+    if len(sys.argv) < 2:
+        print("Error: Argument is not valid")
+        print(WARNING + "Status: --argvnotcorrect" + ENDC)
+        exit()
+    elif len(sys.argv) > 2:
+        print("Error: Too man arguments")
+        print(WARNING + "Status: --overexpectedargvs" + ENDC)
+    else:
+        file_name = sys.argv[1]
+        run = Build()
+        return_code = Build.combine_all(run, file_name)
 
-    run = Build()
-    return_code = Build.combine_all(run, file_name)
-
-    if return_code == 0:
-        print("Completed successfully")
-        print("Status: --noerror")
-    elif return_code == 1:
-        print("Error: Code is not executed")
-        print("Status: --error")
+        if return_code == 0:
+            print("Completed successfully")
+            print(WARNING + "Status: --noerror, --completedsuccessfully" + ENDC)
+        elif return_code == 1:
+            print("Error: Code is not executed")
+            print(WARNING + "Status: --error" + ENDC)
