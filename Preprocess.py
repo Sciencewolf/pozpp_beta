70e173227f12ed7404646a6a7a872587abdb2beb
Sciencewolf <aron17marton@gmail.com>
2022-12-14 22:41:38 +0100

    Implemented all keyword replacement

--- Preprocess.py
+++ Preprocess.py
@@ -2,9 +2,9 @@ from Interpreter import Interpreter
 
 
 class Preprocess:
-    '''print("""
-        Preprocess called
-    """, end="")'''
+    """print(
+        "Preprocess called"
+    , end="") """
 
     def __init__(self):
         self.interpreter = Interpreter()
@@ -24,33 +24,22 @@ class Preprocess:
     def convert_pozpp_to_py(self, file_name: str):
         var = self.interpreter.open_pozpp_file(file_name)
         self.pozpp_file = open(f"{var}", 'r')
-        #print("Var ", var)
-
         self.py_file = open(f'{file_name[:-6]}.py', 'w')
 
         lines = self.pozpp_file.readlines()
-        #print(lines)
-
         words = list(self.KEYWORDS.keys())
-        #print("Words: ", words)
 
-        # Implementation of converting pozpp to py, all keywords
         for line in lines:
             for word in words:
                 if word in line:
-                    # print(word, 'Add', 'line:', line, end=' ')
                     self.ln = line.replace(word, self.KEYWORDS[word])
                     self.py_file.write(self.ln)
-                    # print(self.ln)
             if line.startswith("#"):
                 self.ln = line
                 self.py_file.write(self.ln)
-                # print(self.ln)
             if line.startswith("\n"):
                 self.ln = line
                 self.py_file.write(self.ln)
-
-        print("File name", self.py_file.name)
 
         self.pozpp_file.close()
         self.py_file.close()
