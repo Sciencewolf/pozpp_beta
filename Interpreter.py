5d10ecfcbef75268e79f2f5211bac22a37e2566a
Sciencewolf <aron17marton@gmail.com>
2022-12-15 19:28:26 +0100

    Added get_syntax method

--- Interpreter.py
+++ Interpreter.py
@@ -1,11 +1,18 @@
 print(
     """
-        Welcome to Pozakarpatskiy++ Programming Language
-        Version 0.5 
-        Project started: 11/28/2022
+———————————————————————————————————————————————————————————
+|       Welcome to Pozakarpatskiy++ Programming Language  |
+|       Version 0.5                                       |
+|       -----------                                       |
+|       Get help                                          |
+|       |                                                 |
+|       python main.py --help                             |
+|       or                                                |
+|       python main.py -h                                 |
+|       -----------                                       |
+|       Project started: 11/28/2022                       |
+———————————————————————————————————————————————————————————
     """)
-print('\033[93m' + "Get help python main.py --help" + '\033[0m')
-print('\033[93m' + "Get help python main.py -h" + '\033[0m')
 
 
 class Interpreter:
@@ -31,51 +38,52 @@ class Interpreter:
 
     def get_syntax(self):
         text = """
-        Pozakarpatskiy programming language is based on Python programming language
-        Syntax of Pozakarpatskiy(pozpp) is similar to Python(py), but keywords(not all already) is changed
-            
-        KEYWORDS
-            In Python -> def, main(), if, elif, else, print, input
-            In Pozakarpatskiy -> chislo, holovna, kity, kity_net, ajbo, nekaj, vhod
-            
-        In pozpp this code below is required to write
-            |
-            kity __name__ == "__main__":
-                # methods, fields is here 
-            
-        Python version
-            |
-            if __name__ == "__main__":
-                # calling methods here
-                # fields here
-                
-        Hello, World! Program
-            |
-            kity __name__ == "__main__":
-                nekaj("Hello, World!")
-                
-                
-        List of status strings(Errors)
-            |
-            Zero issue
-                |
-            --noerror
-            [Definition soon]
-            --codeexecuted
-            []
-                |
-            Issues
-                |
-            --fileextnotcorrect
-            []
-            --argvnotcorrect
-            []
-            --overexpectedargvs
-            []
-            --error
-            []
-            --codenotexecuted
-            []
+—————————————————————————————————————————————————————————————————————————————————————————————————————————————
+|        Pozakarpatskiy programming language is based on Python programming language                        |
+|        Syntax of Pozakarpatskiy(pozpp) is similar to Python(py), but keywords(not all already) is changed |
+|                                                                                                           |
+|        KEYWORDS                                                                                           |
+|            In Python -> def, main(), if, elif, else, print, input                                         |
+|            In Pozakarpatskiy -> chislo, holovna, kity, kity_net, ajbo, nekaj, vhod                        |
+|                                                                                                           |
+|        In pozpp this code below is required to write                                                      |
+|            |                                                                                              |
+|            kity __name__ == "__main__":                                                                   |
+|                # methods, fields is here                                                                  |
+|                                                                                                           |
+|        Python version                                                                                     |
+|            |                                                                                              |
+|            if __name__ == "__main__":                                                                     |
+|                # calling methods here                                                                     |
+|                # fields here                                                                              |
+|                                                                                                           |
+|        Hello, World! Program                                                                              |
+|            |                                                                                              |
+|            kity __name__ == "__main__":                                                                   |
+|                nekaj("Hello, World!")                                                                     |
+|                                                                                                           |
+|                                                                                                           |
+|        List of status strings(Errors)                                                                     |
+|            |                                                                                              |
+|            Zero issue                                                                                     |
+|                |                                                                                          |
+|            --noerror                                                                                      |
+|            [Definition soon]                                                                              |
+|            --codeexecuted                                                                                 |
+|            []                                                                                             |
+|                |                                                                                          |
+|            Issues                                                                                         |
+|                |                                                                                          |
+|            --fileextnotcorrect                                                                            |
+|            []                                                                                             |
+|            --argvnotcorrect                                                                               |
+|            []                                                                                             |
+|            --overexpectedargvs                                                                            |
+|            []                                                                                             |
+|            --error                                                                                        |
+|            []                                                                                             |
+|            --codenotexecuted                                                                              |
+|            []                                                                                             |
+—————————————————————————————————————————————————————————————————————————————————————————————————————————————
     """
-
         return text
